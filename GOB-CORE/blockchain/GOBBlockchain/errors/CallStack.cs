3ab694b71dd0b1fcb515b1b0ab92ef3d4b3f4f0e
김민수 <minsoo5403@gmail.com>
2019-04-10 18:05:40 +0900

    이번엔 되나 ..

---
+++ GOB-CORE/blockchain/GOBBlockchain/errors/CallStack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GOBBlockchain.errors
+{
+    interface ICallStacker
+    {
+        CallStack GetCallStack(int skip, int depth);
+    }
+
+    class CallStack
+    {
+        UInt32[] Stacks;
+    }
+}
---
+++ GOB-CORE/blockchain/GOBBlockchain/events/Event.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GOBBlockchain.events
+{
+    public enum EventType
+    {
+        EventSaveBlock = 0,
+        EventReplyTx = 1,
+        EventBlockPersistCompleted = 2,
+        EventNewInventory = 3,
+        EventNodeDisconnect = 4,
+        EventSmartCode = 5,
+        EventNodeConsensusDisconnect = 6
+    }
+
+    class Event
+    {
+        RWMutex m;
+        Dictionary<Int16, Dictionary<Subscriber, EventFunc>> subscribers;
+    }
+
+    class RWMutex
+    {
+        // w Mutex
+        UInt32 writerSem;
+        UInt32 readerSem;
+        UInt32 readerCount;
+        UInt32 readerWait;
+    }
+}
