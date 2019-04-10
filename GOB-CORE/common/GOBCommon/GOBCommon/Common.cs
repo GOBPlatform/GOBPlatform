f1982ef27473ebff1ec059b8c7eb702662602b93
이재영 <iscream6@gmail.com>
2019-04-10 14:44:48 +0900

    ...

--- GOB-CORE/common/GOBCommon/GOBCommon/Common.cs
+++ GOB-CORE/common/GOBCommon/GOBCommon/Common.cs
@@ -6,5 +6,13 @@ namespace GOBCommon
 {
     public class Common
     {
+        public UInt64 GetNonce()
+        {
+            Random rnd = new Random();
+            
+            UInt64 nonce;
+
+            return nonce;
+        }
     }
 }
